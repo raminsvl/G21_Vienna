651c846d7202a3ae11ed6b6761e7b78fe5829d0c
Hanstadd <niha034@student.kristiania.no>
2023-06-13 14:24:47 +0200

    .

--- src/js/leaderboard.js
+++ src/js/leaderboard.js
@@ -1,40 +1,62 @@
-import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-
-const Leaderboard = () => {
-  const [profiles, setProfiles] = useState([]);
-
-  useEffect(() => {
-    displayProfiles();
-  }, []);
-
-  async function displayProfiles() {
-    try {
-      const response = await fetch('http://localhost:5000/Munch');
-      if (response.ok) {
-        const profilesData = await response.json();
-        setProfiles(profilesData);
-      } else {
-        console.error('Failed to fetch profiles:', response.status);
-      }
-    } catch (error) {
-      console.error('Error while fetching profiles:', error);
-    }
+function Leaderboard(profiles) {
+    // Convert the profiles object to an array
+    const profilesArray = Object.values(profiles);
+  
+    // Sort the profiles by score in descending order
+    const sortedProfiles = profilesArray.sort((a, b) => b.score - a.score);
+  
+    // Create an HTML table element
+    const table = document.createElement('table');
+  
+    // Create the table header row
+    const headerRow = table.insertRow();
+    const headerNames = ['Name', 'Score', 'Time', 'Shared Code'];
+  
+    headerNames.forEach(headerName => {
+      const headerCell = document.createElement('th');
+      headerCell.textContent = headerName;
+      headerRow.appendChild(headerCell);
+    });
+  
+    // Create table rows for each profile
+    sortedProfiles.forEach(profile => {
+      const row = table.insertRow();
+      const { name, score, time, sharedCode } = profile;
+  
+      const nameCell = row.insertCell();
+      nameCell.textContent = name;
+  
+      const scoreCell = row.insertCell();
+      scoreCell.textContent = String(score);
+  
+      const timeCell = row.insertCell();
+      timeCell.textContent = String(time);
+  
+      const sharedCodeCell = row.insertCell();
+      sharedCodeCell.textContent = String(sharedCode);
+    });
+  
+    // Append the table to the document body
+    document.body.appendChild(table);
   }
-
-  return (
-    <div>
-      <h1>Leaderboard</h1>
-      <ul>
-        {profiles.map((profile) => (
-          <li key={profile.id}>
-            Name: {profile.Name}, Score: {profile.Score}, Time: {profile.Time}, sharedCode: {profile.sharedCode}
-          </li>
-        ))}
-      </ul>
-      <Link to="/game">Go Back to Game</Link>
-    </div>
-  );
-};
-
-export default Leaderboard;
+  
+  // Make a fetch request to retrieve the response data
+  fetch('http://localhost:5006/Munch')
+    .then(response => response.json())
+    .then(data => {
+      // Convert the score, time, and sharedCode to numbers
+      const profiles = data.map(profile => ({
+        ...profile,
+        score: parseInt(profile.score),
+        time: parseInt(profile.time),
+        sharedCode: parseInt(profile.sharedCode)
+      }));
+  
+      Leaderboard(profiles); // Pass the response data to the Leaderboard function
+    })
+    .catch(error => {
+      console.error(error);
+    });
+  
+  export default Leaderboard;
+  
